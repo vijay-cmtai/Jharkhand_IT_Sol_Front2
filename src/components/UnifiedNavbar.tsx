cf11f28f659a2ce564988cb7b8799ab260ea04bd
vijay-cmtai <vijay.maurya@completrix.com>
2025-06-11 15:05:35 +0530

    Update UnifiedNavbar.tsx

--- src/components/UnifiedNavbar.tsx
+++ src/components/UnifiedNavbar.tsx
@@ -163,7 +163,7 @@ const localIndustriesDataArray: LocalIndustryDataItem[] = [
   },
   {
     id: "manufacturing",
-    name: "Manufacturing & IoT",
+    name: "Manufacturing ",
     icon: Factory,
     description: "Driving efficiency and innovation...",
     imageUrl: I7,
